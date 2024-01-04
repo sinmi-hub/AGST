d44d3a7763d194d8c3b835a1e9a307eb6c6ce957
sinmi <similoluwa232@gmail.com>
2024-01-04 18:59:16 -0500

    Fixed bug in app configuration

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -2,7 +2,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_mongoengine import MongoEngine
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
+from flask_login import LoginManager, current_user
+
 
 # local -- created objects
 from .client import SchoolClient
@@ -18,6 +19,10 @@ school_client = SchoolClient()
 # ------------------ importing blueprints to be used --------------
 from .content.routes import content
 from .users.routes import users
+
+# custom 404
+def custom_404(e):
+    return render_template("404.html"), 404
 
 
 # Todo
@@ -36,10 +41,11 @@ def create_app(test_config=None):
     login_manager.init_app(app)
     bcrypt.init_app(app)
 
-    # registering the blueprint to the created app
+    # registering the blueprint to the created app and creating a custom error page
     app.register_blueprint(users)
     app.register_blueprint(content)
+    app.register_error_handler(404, custom_404)
 
-
+    login_manager.login_view = "users.login"
     
     return app
