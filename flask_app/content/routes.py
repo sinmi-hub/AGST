2d23cd11a6a241816ecbb7b4f8271ac563fb3f40
sinmi <similoluwa232@gmail.com>
2024-01-04 19:11:50 -0500

    Bug fix for account registration to login redirection

--- flask_app/content/routes.py
+++ flask_app/content/routes.py
@@ -24,8 +24,50 @@ def index():
 @content.route("/search-results/<query>", methods=["GET"])
 def query_results(query):
     try:
-        results = school_client.search_by_name(query)
+        schools = school_client.search_by_name(query)
+       
+        if len(schools) == 0:
+            return "No school found by that name"
     except ValueError as e:
         return render_template("query.html", error_msg=str(e))
 
-    return render_template("query.html", results=results)
+    return render_template("query.html", schools=schools)
+
+#TODO. Give more information about school detail. Copied from P4 to use as starting template. Intend to use the funct
+@content.route("/content/<schls>", methods=["GET", "POST"])
+def school_detail(schls):
+    try:
+        result = school_client.display_schools(schls)
+        return result
+    except ValueError as e:
+        return render_template("movie_detail.html", error_msg=str(e))
+
+    # form = MovieReviewForm()
+
+    # if form.validate_on_submit():
+    #     review = Review(
+    #         commenter=current_user._get_current_object(),
+    #         content=form.text.data,
+    #         date=current_time(),
+    #         imdb_id=movie_id,
+    #         movie_title=result.title,
+    #     )
+
+    #     review.save()
+
+    #     return redirect(request.path)
+
+    # reviews = Review.objects(imdb_id=movie_id)
+
+    # for review in reviews:
+    #     if review.commenter:
+    #         reviewer_user = User.objects(username=review.commenter.username).first()
+    #         if reviewer_user:
+    #             review.image = get_b64_img(reviewer_user.username)
+    #         else:
+    #             review.image = None  # This is probably already taken care of
+
+
+    # return render_template(
+    #     "movie_detail.html", form=form, movie=result, reviews=reviews
+    # )
